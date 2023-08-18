2c8970235987b5de1ac44aaca7301f5ecd98c8c3
Shin <shinvitiya@gmail.com>
2023-08-18 23:21:29 +0530

    update next config file

--- next.config.js
+++ next.config.js
@@ -1,19 +1,33 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-      appDir: true,
-      serverComponentsExternalPackages: ["mongoose"],
-    },
-    images: {
-      domains: ['lh3.googleusercontent.com'],
-    },
-    webpack(config) {
-      config.experiments = {
-        ...config.experiments,
-        topLevelAwait: true,
-      }
-      return config
-    }
-  }
-  
-  module.exports = nextConfig
+  experimental: {
+    appDir: true,
+    serverComponentsExternalPackages: ["mongoose"],
+  },
+  images: {
+    domains: ['lh3.googleusercontent.com'],
+  },
+  webpack(config) {
+    config.experiments = {
+      ...config.experiments,
+      topLevelAwait: true,
+    };
+    return config;
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=60, s-maxage=60', // Adjust cache settings as needed
+          },
+        ],
+      },
+    ];
+  },
+  // Add other necessary configuration options here
+};
+
+module.exports = nextConfig;
